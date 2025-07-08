b713dc5c94cee51317f6ef9d2cbcb95beee441d1
eliDaniel007 <elisenyo22@gmail.com>
2025-07-07 21:59:32 -0400

    fix: base Vite pour GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/moderant-brvm/',
   plugins: [react()],
   build: {
     // Ignorer les erreurs TypeScript pendant le build
